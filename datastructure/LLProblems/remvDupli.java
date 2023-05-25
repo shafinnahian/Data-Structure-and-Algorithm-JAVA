85672693232e9e7eb41380eecfd2bd02eeb0b4a8
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-05-25 22:10:17 +0600

    solve leetcode 83

---
+++ datastructure/LLProblems/remvDupli.java
@@ -0,0 +1,31 @@
+package datastructure.LLProblems;
+
+public class remvDupli {
+    
+}
+/
+//   Definition for singly-linked list.
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        
+        ListNode node = head;
+
+        while (node.next != null){
+            if (node.val == node.next.val){
+                node.next = node.next.next;
+            } else{
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
