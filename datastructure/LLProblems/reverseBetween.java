4f2f02691b45f45bc3253e57b5a4c734acd73fe3
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-05-28 08:37:06 +0600

    add guide and link problem + companies

--- datastructure/LLProblems/reverseBetween.java
+++ datastructure/LLProblems/reverseBetween.java
@@ -1,5 +1,6 @@
 package datastructure.LLProblems;
 
+//https://leetcode.com/problems/reverse-linked-list-ii/
 
 public class reverseBetween {
     ListNode head;
@@ -17,17 +18,18 @@ public class reverseBetween {
         ListNode present = head;
         ListNode prev = null;
 
-        for (int i = 0; present != null && i< left - 1; i++){
+        for (int i = 0; present != null && i< left - 1; i++){   //bringing prev to last node before sub-list and pres at first node
             prev = present;
             present = present.next;
         }
 
-        ListNode last = prev;
-        ListNode newEnd = present;
+        ListNode last = prev;   //being the last node before the sub-list begins
+        ListNode newEnd = present;  //being the first node of the sublist before reversal, last node after reversal (connects to the tail *tail doesn't exist*)
 
         //reverse
         ListNode next = present.next;
-        for(int i = 0; present != null && i < right - left +1; i++){
+        for(int i = 0; present != null && i < right - left +1; i++){ //iterates the prev, pres and next nodes through the sub-set. 
+                                                                    //prev ends at last node of sub-set, pres at first node after subset
             present.next = prev;
             prev = present;
             present = next;
@@ -36,10 +38,10 @@ public class reverseBetween {
         }
 
         if(last != null){
-            last.next = prev;
+            last.next = prev;   //connects the reverse subset to "last" node (the last node before subset started)
         } else head = prev;
 
-        newEnd.next = present;
+        newEnd.next = present;  //connects the last node of subset to the first node After the subset ended
         return head;
     }
 }
