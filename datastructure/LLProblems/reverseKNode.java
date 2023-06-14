9ad3dab5f113c2088ba6b6cca7d2d68c50e7226a
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-06-14 20:31:08 +0600

    add LC25 reverseKNodes

---
+++ datastructure/LLProblems/reverseKNode.java
@@ -0,0 +1,50 @@
+package datastructure.LLProblems;
+
+// Leetcode 25
+// https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+public class reverseKNode {
+     public ListNode reverseKGroup(ListNode head, int k){
+        if (k == 1 || head == null) return head;
+        if (head.next.next == null){
+            ListNode curr = head.next;
+            ListNode prev = head;
+
+            curr.next = prev;
+            prev.next = null;
+            head = curr;
+
+            return head;
+        }
+
+        ListNode current = head;
+        ListNode prev = null;
+
+        while(true){
+            ListNode last = prev;
+            ListNode newLast = current; 
+
+            ListNode next = current.next;
+            for(int i = 1; current.next != null && i <= k; i++){
+                current.next = prev;
+                prev = current;
+                current = next;
+    
+                if( next != null) next = next.next;
+            }
+    
+            if(last != null){
+                last.next = prev;
+            } else head = prev;
+    
+            newLast.next = current;
+
+            if(current.next == null) break;
+
+            prev = newLast;        
+        }
+        return head;
+
+    }
+    
+}
