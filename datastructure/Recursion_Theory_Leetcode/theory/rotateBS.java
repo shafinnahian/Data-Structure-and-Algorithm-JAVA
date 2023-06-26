b3506fe5d6411be2d826625306f290577c48ddb1
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-06-26 07:13:54 +0600

    add rotated binary search

---
+++ datastructure/Recursion_Theory_Leetcode/theory/rotateBS.java
@@ -0,0 +1,19 @@
+package Recursion_Theory_Leetcode.theory;
+
+public class rotateBS {
+    
+    static int search(int[] arr, int target, int s, int e){
+    if (s  > e) return -1;
+
+    int m = s + ( e - s )/2;
+    if (arr[m] == target) return m;
+
+    if (arr[s] <= arr[m]){
+        if (target >= arr[s] && target <= arr[m]) return search(arr, target, s, m-1);
+    } else return search(arr, target, m+1, e);
+
+    if (target >= arr[m] && target <= arr[e]) return search(arr, target, m+1, e);
+
+    return search(arr, target, s, m-1);
+}
+}
