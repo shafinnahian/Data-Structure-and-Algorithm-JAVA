c928c53df94e799ff3f3e9d1b288f69c3d52ea4b
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-06-28 08:40:28 +0600

    Optimize pivot point w/ median of three val

--- datastructure/Recursion_Theory_Leetcode/theory/quickSortRecur.java
+++ datastructure/Recursion_Theory_Leetcode/theory/quickSortRecur.java
@@ -48,4 +48,54 @@ public class quickSortRecur {
         quickSort(arr, low, e);     //LHS
         quickSort(arr, s, high);    //RHS
     }
+
+    //Optimized version, with getPivotIndex funciton selecting the median of three value - nullifying the worst case scenario
+    
+    // static void quickSort(int[] arr, int low, int high) {
+    //     if (low >= high) return;
+
+    //     int pivotIndex = getPivotIndex(arr, low, high);
+    //     int pivot = arr[pivotIndex];
+
+    //     // Swap pivot with the last element
+    //     int temp = arr[pivotIndex];
+    //     arr[pivotIndex] = arr[high];
+    //     arr[high] = temp;
+
+    //     int i = low - 1;
+    //     for (int j = low; j < high; j++) {
+    //         if (arr[j] < pivot) {
+    //             i++;
+    //             // Swap elements at i and j
+    //             int temp2 = arr[i];
+    //             arr[i] = arr[j];
+    //             arr[j] = temp2;
+    //         }
+    //     }
+
+    //     // Move pivot to its correct position
+    //     int temp3 = arr[i + 1];
+    //     arr[i + 1] = arr[high];
+    //     arr[high] = temp3;
+
+    //     int pivotPosition = i + 1;
+
+    //     quickSort(arr, low, pivotPosition - 1);  // Sort left subarray
+    //     quickSort(arr, pivotPosition + 1, high); // Sort right subarray
+    // }
+
+    // static int getPivotIndex(int[] arr, int low, int high) {
+    //     int mid = low + (high - low) / 2;
+    //     int a = arr[low];
+    //     int b = arr[mid];
+    //     int c = arr[high];
+
+    //     // Return the index of the median value
+    //     if ((a <= b && b <= c) || (c <= b && b <= a))
+    //         return mid;
+    //     else if ((b <= a && a <= c) || (c <= a && a <= b))
+    //         return low;
+    //     else
+    //         return high;
+    // }
 }
